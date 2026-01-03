a0a517e4dea4f79b9d7f0dd62fdb780bbd2d76df
yaomeng1 <27053507+yaomeng1@users.noreply.github.com>
2026-01-03 23:02:25 +0800

    Update fixprob_temp_rhoC.m

--- fixprob_temp_rhoC.m
+++ fixprob_temp_rhoC.m
@@ -6,7 +6,7 @@ tic
 warning('off');
 global k_ini n ratio
 
-load('rr_200_k6_4_snapmatrix.mat');
+load('sf_200_k6_4_snapmatrix.mat');
 limit_idx = 100;
 
 delta=0.01;
@@ -14,15 +14,9 @@ N=200;
 delta_t=1;
 time_length=3000;
 time_array=0:delta_t:time_length;     % time_array must bt row vector
-% theory prob density and P[t<T_coal]
-% [a_mat,tau_a,b_mat,tau_b] = prob_fit_2expon(prob_time_mat,P_coal_t);
-% Center_tau = tau_array(P_coal_t,round(n));
-%%
 
 b_array = 7.407;     % b must be scalar 
-% %b*=7.407 for g=1e3 
-% %b*= 6.25 for g=2e3 
-% %b*= 4.95 for g=1e4 
+
 
 p_final_fix_sf = zeros(1,length(b_array));
 for b_index=1:length(b_array)
@@ -63,13 +57,8 @@ for idx=1:iter_num
     
     for k = 1:n-1
         k_ini=k;
-%         if k<n/2
         prob_vec_sub(k,end) = fix_prob(k)-fix_prob(k)*exp(-g/Center_tau(round(n-k)));
         prob_vec_sub(k,1) = 1-fix_prob(k)-(1-fix_prob(k))*exp(-g/Center_tau(round(k)));
-%         else
-%         prob_vec_sub(k,end) = fix_prob(k)-fix_prob(k)*exp(-g/Center_tau(n-k));
-%         prob_vec_sub(k,1) = 1-fix_prob(k)-(1-fix_prob(k))*exp(-g/Center_tau(end));
-%         end 
         pn = prob_vec_sub(k,end);
         p0 = prob_vec_sub(k,1);
 
@@ -78,7 +67,6 @@ for idx=1:iter_num
             a=0;
         else
             ratio = n*(mFreq_g(k)-pn)/(1-p0-pn);
-%             d = fzero(@root_b,0.5);
             [d,fval] = fsolve(@root_b,0.95); % initial piont is 0.95
             if d>0
             a=(1-p0-pn)*(1 - d)/(1 + d - d^k - d^(-k + n));
@@ -93,7 +81,6 @@ for idx=1:iter_num
 
 
     end
-%     prob_vec_sub(prob_vec_sub<0)=0;
     for i=0:N
         % the total net contain i cooperator
         pi = prob_idx(i+1); 
@@ -123,11 +110,9 @@ for idx=1:iter_num
 end
 
 prob_total = prob_total(1:idx+1,:); % get rid of zeros of unused index
-% save('./prob_total_vec_re.mat','prob_total');
 p_final_fix_sf(b_index) = prob_total(end,end);
 end
 
 sf_fixprob_b = [b_array;p_final_fix_sf];
 
-% save('./re_fixprob_b_array_dt_g1e3.mat','sf_fixprob_b');
 
